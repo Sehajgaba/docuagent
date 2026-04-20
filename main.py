84f27ea1bf5a36e9910de9362fff5bcf02a403a0
Sehajgaba <sehajgabaa@gmail.com>
2026-04-20 22:13:53 +0530

    added vector db search

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 
 import pdfplumber
 import json
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('all-MiniLM-L6-v2')
 with pdfplumber.open("RIL-Integrated-Annual-Report-2024-25.pdf") as pdf:
-    first_page = pdf.pages[0]
+    first_page = pdf.pages[56]
     table_data = first_page.extract_table()
 
 # Create the empty dictionary
-financial_json = {}
+vector_database = []
 
 # Loop through the data, skipping the first row (headers)
 for row in table_data[1:]:
-    # Notice the indentation. This code lives INSIDE the loop.
     key = row[0]
     value = row[1]
-    financial_json[key] = value
-
+    search_text = f"{key}: {value}"
+    embedding = model.encode(search_text)
+    
+    # Store both the text and the math together!
+    row_data = {
+        "text": search_text,
+        "vector": embedding
+    }
+    vector_database.append(row_data)
 # Print the final dictionary
-print(financial_json)
+print(len(vector_database))
+
+user_query = "What is the total cash?"
+query_vector = model.encode(user_query)
+
+# We will keep track of the highest score and the best text
+best_score = 0
+best_answer = ""
+
+# Loop through our database to find the match
+for item in vector_database:
+    # Compare the user's question to the current row's vector
+    # util.cos_sim returns a complex tensor, .item() converts it to a standard Python decimal
+    similarity_score = util.cos_sim(query_vector, item["vector"]).item()
+    
+    # If this score is the highest we've seen, save it!
+    if similarity_score > best_score:
+        best_score = similarity_score
+        best_answer = item["text"]
+
+print(f"Top Match: {best_answer}")
+print(f"Confidence Score: {best_score}")
